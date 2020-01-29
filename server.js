05715ffba0192b0f1794558cc6e004bcf028f4e4
leejiho <jiholee@kpu.ac.kr>
2020-01-30 08:17:33 +0900

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const { createCanvas, loadImage } = require('canvas')
+const fs = require('fs')
+const stream = require('stream')
+const express = require('express')
+const app = express()
+
+app.use(function (req, res, next) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  console.log(ip)
+  next()
+})
+
+const port = 4500
+ 
+app.use('/img', express.static(__dirname + '/img'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + 'test.html')
+})
+ 
+app.listen(port)
+console.log(`listening at ${port}!`)
