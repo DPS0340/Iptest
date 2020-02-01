4f3ce24961d893d1d1c6e7d7d06456fcf0ebf3eb
leejiho <jiholee@kpu.ac.kr>
2020-02-01 09:15:00 +0900

    commit

--- server.js
+++ server.js
@@ -13,16 +13,18 @@ if (!fs.existsSync(logDir)) {
 
 app.use(function (req, res, next) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const url = `${req.protocol}://${req.get('host')}${req.url}`
   const now = new Date().toISOString().
   replace(/T/, ' ').
   replace(/\..+/, '')
-  console.log(`${now}: ${ip}`)
-  fs.appendFileSync(logtxt, `${now}: ${ip}\n`)
+  console.log(`${now}\nip: ${ip}\nurl: ${url}\n`)
+  fs.appendFileSync(logtxt, `${now}\nip: ${ip}\nurl: ${url}\n\n`)
   next()
 })
 
 const port = 4500
- 
+
+
 app.use('/img', express.static(__dirname + '/img'))
 
 app.get('/', function (req, res) {
